3f2165a828444be15c16d7a085666a5a3607df96
Kate KyuWon <lkwon0302@gmail.com>
2015-08-19 00:10:38 +0900

    random desk

---
+++ deskDB.py
@@ -0,0 +1,8 @@
+desks = [
+	"static/2.1yesol.jpg",
+	"static/2.2heongjin.jpg",
+	"static/2.3dahye.jpg",
+	"static/2.4yunu.jpg",
+	"static/2.5jungbong.jpg",
+	"static/2.6taeho.jpg",
+]
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+
+
+import json
+import random
+from deskDB import desks
+
+from flask import (Flask, render_template, redirect, url_for, request, make_response, flash)
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def home():
-	return render_template("home.html")
+	desk = random.choice(desks)
+	return render_template("home.html", NextDesks = desk)
 
 
 app.run(debug=True, port=3000, host='0.0.0.0')
